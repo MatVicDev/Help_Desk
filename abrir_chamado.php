f56b8560543448ba9aa325712d05546b5cbe82ec
MatVicDev <matheus@teste.com>
2021-09-21 21:00:56 -0300

    Diminuindo a redundância do código com o require_once

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -1,10 +1,4 @@
-<?php
-
-session_start();
-if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] == 'NAO')
-  header('Location: index.php?erro=permissao')
-
-?>
+<?php require_once "valida_acesso.php" ?>
 
 <html>
   <head>
